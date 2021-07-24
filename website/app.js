21456dda00eb89dd5afa07550f81905a65a8a454
Ahmed M.Osman <64021350+akiid777@users.noreply.github.com>
2021-07-24 15:31:14 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -30,17 +30,15 @@ console.log("the zip code is" , zipCode)
     // lets fetch the api with await to make sure that is everything is working fine and save it in string to use it:
     const weatherAPIDataInJSON = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${theWeatheAppKey}&units=metric`);
         const weatherAPIData = await weatherAPIDataInJSON.json();
-        console.log(weatherAPIData)
         const temprture = weatherAPIData.main.temp;
         //Bounce points (I add extra information like were are this zip code and humidity to make this api more usable)
         const country = weatherAPIData.sys.country;
         const place = weatherAPIData.name;
         const humidity = weatherAPIData.main.humidity;
         console.log(temprture) // I want to delete it but i think it will be usefull for you to check that two values are idntical here and in the index.html
-        return serverSection(temprture,contentOfFeeling,country,place,humidity)
-      });
-
-
+        return serverSection (temprture,contentOfFeeling,country,place,humidity) // {temprture,contentOfFeeling,country,place,humidity};
+      })
+      
 // server section: 
 // in this section the data will go out and in the server and save it so I can use it in display my data
 // Important: this is the most importatnt function that will convert the date to readable and store it. without it we can't read.
@@ -64,14 +62,15 @@ async function serverSection (temprture,contentOfFeeling,country,place,humidity)
                 place: place,
                 humidity: humidity
             }),
-          });
+          }).then((finalFormOfData) =>{displayResults(finalFormOfData)})
+          
           // get Reqest Data.
           const serverRes = await fetch("/getReqData", {
           credentials: 'same-origin'
           });
+           
           // the final form of data.
           const finalFormOfData = await serverRes.json();
-            return displayResults(finalFormOfData);
             };
             //the function to the display the data.
 async function displayResults(finalFormOfData) {
@@ -82,7 +81,7 @@ const fetchTheData = await fetch ('/getReqData');
   let displayDate = document.getElementById("date").innerHTML = "Today date is " + theData.date;
   // Bounce points (I convert the C to F inside the function and did not used extranal function. I think this is good way. I did not want the code to be longer.
   // Bounce points ( toFix to make the 1 decimal we not want to make the temp with alot of decimals, right ?)
-  let displayTemp = document.getElementById("temp").innerHTML = "Today temprature is " +theData.temp.toFixed(1)+ " °C which equal to " + (theData.temp*9/5 + 32).toFixed(1) + " °F and The humidity is "+theData.humidity + "%";
+  let displayTemp = document.getElementById("temp").innerHTML = "Today temprature is " + theData.temp.toFixed(1)+ " °C which equal to " + (theData.temp*9/5 + 32).toFixed(1) + " °F and The humidity is "+theData.humidity + "%";
   let displayContent = document.getElementById("content").innerHTML = "I heared that you are " +theData.content + " . So did I.";
   let displayCountry = document.getElementById("country").innerHTML = "This zip code you enter is in " +theData.place + " , "+theData.country;
   // if error happan.
